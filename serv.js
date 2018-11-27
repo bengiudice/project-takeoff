9aab5319a9c18c1167d7abbdb5f3b0058c1f98b6
Ben Giudice <ben.giudice@energymadeclean.com>
2018-11-27 17:17:26 +0800

    nogauge

--- serv.js
+++ serv.js
@@ -9,12 +9,3 @@ http.createServer(function (request, response) {
     });
 }).listen(8125);
 console.log('server running (port 8125)...');
-
-http.createServer(function (request, response) {
-    console.log('received request...');
-    fs.readFile('gauge.html', function(error, content) {
-        response.writeHead(200, { 'Content-Type': 'text/html'});
-        response.end(content, 'utf-8');
-    });
-}).listen(8126);
-console.log('server running (port 8126)...');
