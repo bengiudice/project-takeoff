9da2d4d7f4556a57065532eb706c2269cda7a0e5
Ben Giudice <ben.giudice@energymadeclean.com>
2018-11-27 17:29:55 +0800

    dynPort

--- serv.js
+++ serv.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
+let port = process.env.PORT || 80;
+
+
 http.createServer(function (request, response) {
     console.log('received request...');
     fs.readFile('web.html', function(error, content) {
         response.writeHead(200, { 'Content-Type': 'text/html'});
         response.end(content, 'utf-8');
     });
-}).listen(80);
-console.log('server running (port 8125)...');
+}).listen(port);
+console.log('server running (port ' + port + ')...');
